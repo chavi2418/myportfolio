fd541377e0f9cfda0e036eff4148225d29ad9ef7
unknown <chavigarg2016@gmail.com>
2021-09-11 11:12:51 +0530

    updated branch

--- src/App.js
+++ src/App.js
@@ -64,3 +64,10 @@ function App() {
 }
 
 export default App;
+
+
+ // "start": "react-scripts start",
+    // "build": "react-scripts build",
+    // "test": "react-scripts test",
+    // "eject": "react-scripts eject",
+    // "predeploy": "npm run build",
